dd77c106f6109abd657719c1bda29f0466fe56a8
Hyan <hyanmarcos15@gmail.com>
2023-06-15 21:38:15 -0300

    little more backend

--- src/server.js
+++ src/server.js
@@ -2,5 +2,20 @@ const express = require("express")
 
 const app = express()
 
+app.get("/message/:id/:user", (request, response) => {
+    const { id, user } = request.params
+
+    response.send(`
+    Id da mensagem: ${id}.
+    Para o usuário: ${user}.
+    `)
+})
+
+app.get("/users", (request, response) => {
+    const { page, limit, title } = request.query
+
+    response.send(`Página: ${page}. Mostrar: ${limit}. Título: ${title}`)
+})
+
 const PORT = 3333
 app.listen(PORT, () => console.log(`Server is rinning on port ${PORT}`))
